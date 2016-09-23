13f5bd6a3e0e51a89f414877088cfa703f5d9a45
amiteshmahajan <121amitesh@gmail.com>
2016-09-23 01:07:47 -0600

    commiting changes

--- src/FishTank/FishTank.java
+++ src/FishTank/FishTank.java
@@ -1,21 +1,24 @@
 package FishTank;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
 
 public class FishTank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Species goldFish = new Species();
-		Species s2 = new Species();
-		Species s3 = new Species();
-		Species s4 = new Species();
-		Species s5 = new Species();
-		Species s6 = new Species();
-		Species s7 = new Species();
+		Species goldFish = new Species("goldfish");
+		Species s2 = new Species("s2");
+		Species s3 = new Species("s3");
+		Species s4 = new Species("s4");
+		Species s5 = new Species("s5");
+		Species s6 = new Species("s6");
+		Species s7 = new Species("s7");
 		goldFish.SetspeciesName("goldfish");
 		s2.SetspeciesName("s2");
 		s3.SetspeciesName("s3");
@@ -42,10 +45,10 @@ public class FishTank {
 		// System.out.println(s4.fishCount);
 		Food food = new Food();
 		Food.Time t = null;
-		food.feed(f1,goldFish, 10, t.evening);
-		food.feed(f2,goldFish, 10, t.evening);
-		food.feed(f3,goldFish, 10, t.evening);
-		food.feed(f4,goldFish, 10, t.evening);
+		food.feed(f1, goldFish, 10, t.evening);
+		food.feed(f2, goldFish, 10, t.evening);
+		food.feed(f3, goldFish, 10, t.evening);
+		food.feed(f4, goldFish, 10, t.evening);
 
 		double fedPerDay = 0.0d;
 		System.out.println("Enter your choice:");
@@ -58,37 +61,34 @@ public class FishTank {
 		case 1: {
 			System.out.println("total fishes:" + Fish.totalFish + "\ntotal food eaten by all the fishes:"
 					+ Food.foodQuantity + "gms");
-			float avg = Food.foodQuantity / Fish.totalFish;
-			System.out.println("Average food eaten by indivudual fish:" + avg + "gms");
+			float avg =(float) Food.foodQuantity / Fish.totalFish;
+			System.out.println("Average food eaten by indivudual fish:" + avg + "gms");break;
 
 		}
 		case 2: {
 			System.out.println("Enter specie name");
 			String sName = sc.next();
-			float foodByspecie=0.0f;
-			foodByspecie=goldFish.Food/goldFish.fishCount;
-			//			int foodEatenBySpecie = 0;
-//			if (Fish.hmap.containsKey(sName)) {
-//				Iterator it = Fish.hmap.entrySet().iterator();
-//				while (it.hasNext()) {
-//					Map.Entry pair = (Map.Entry) it.next();
-//					Fish t1 = new Fish();
-//					t1 = (Fish) pair.getValue();
-//					foodEatenBySpecie += t1.getFoodEaten();
-//					System.out.println(pair.getKey() + " = " + pair.getValue());
-//					it.remove(); // avoids a ConcurrentModificationException
-//				}
-
-//			} else {
-//				System.out.println("no such specie found in fish tank");
-//			}
+			Species s = Species.hmap.get(sName);
+			System.out.println(s.SpecieName);	
+			if (Species.hmap.containsKey(sName)) 
+			{
+				System.out.println("specie found!");
+//				Species s = Species.hmap.get(sName);
+				System.out.println(s.SpecieName);
+				float avgSpecies = s.Food / s.fishCount;
+				System.out.println("Avg food eaten by fishes of " + sName + " is =" + avgSpecies);
+			} 
+			else 
+			{
+				System.out.println("specie not found in the tank");
+			}break;
 		}
-		case '3': {
-
-		}
-		case '4': {
-
-		}
+//		case '3': {
+//
+//		}
+//		case '4': {
+//
+//		}
 		default: {
 			System.out.println("invalid input");
 		}
--- src/FishTank/Species.java
+++ src/FishTank/Species.java
@@ -1,17 +1,35 @@
 package FishTank;
 
+import java.util.HashMap;
 
 public class Species {
 	//public int SpeciesCount;
 	public int fishCount=0;
-	String SpecieName;
+	public int Food;
+	public static HashMap<String,Species> hmap=new HashMap<String,Species>();
+
+	public String SpecieName;
 	void SetspeciesName(String s){
 		this.SpecieName=s;
+		System.out.println(SpecieName);
+
+		hmap.put(this.toString(), this);
+	}
+	Species (String s){
+		this.SpecieName=s;
+		
+	}
+	Species(){
+		
+		hmap.put(this.toString(), this);
+		SpecieName=this.toString();
+		System.out.println(this.toString());
+		System.out.println("inside default constructor");
 	}
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return super.toString();
+		return SpecieName;
 	}
 
 }
