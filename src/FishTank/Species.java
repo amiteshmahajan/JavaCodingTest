994b18a779a62b555e9f29ba1d57dcdf046c66ec
amiteshmahajan <121amitesh@gmail.com>
2016-09-22 23:26:33 -0600

    commiting partial program done till now

---
+++ src/FishTank/Species.java
@@ -0,0 +1,17 @@
+package FishTank;
+
+public class Species {
+	//public int SpeciesCount;
+	public int fishCount=0;
+	String SpecieName;
+	void SetspeciesName(String s){
+		this.SpecieName=s;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
